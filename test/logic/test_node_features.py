f13ac5139fd2f252548b6d5159884dc55a1af15c
Sam-prog-sudo <61703841+Sam-prog-sudo@users.noreply.github.com>
2020-07-15 21:29:29 +0200

    ajout de test_register_node pour la logique

--- test/logic/test_node_features.py
+++ test/logic/test_node_features.py
@@ -1,19 +1,20 @@
-# import pytest
+import pytest
 
-# from app.chaine.blockchain import Blockchain
-
-
-# class testRegisterNodes():
-
-#     def test_valid_nodes(self):
-
-#         blockchain = Blockchain()
-#         blockchain.register_node('http://192.168.0.1:5000')
-#         assert blockchain.nodes == ['http://192.168.0.1:5000']
-
-#     def test_malformed_nodes(self):
-#         blockchain = Blockchain()
-#         with pytest.raises(AssertionError):
-#             blockchain.register_node('http192.168.0.1:5000')
+from app.chaine.blockchain import Blockchain
 
 
+@pytest.mark.parametrize('some_adresses, striped_adresses, expected', [
+    ('http://192.168.0.1:5000', '192.168.0.1:5000', True),
+    ('http192.168.0.1:5000', '192.168.0.1:5000', False),
+    ('192.168.0.1:5000', '192.168.0.1:5000', False),
+    ('salut :)', 'salut :)', False),
+    ]
+)
+def test_register_node(some_adresses, striped_adresses, expected):
+    bc = Blockchain()
+    bc.register_node(some_adresses)
+    assert (striped_adresses in bc.nodes) is expected
+
+# def test_valid_chain
+
+# def test_algo_consensus
